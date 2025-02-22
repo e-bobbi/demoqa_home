17374f5cca312c050b492ce4602214026e9b3db1
Евгений <enbob.tester@gmail.com>
2025-02-22 17:46:01 +0300

    conftest1.py

--- conftest1.py
+++ conftest1.py
@@ -1,4 +1,4 @@
-#### g. Создайте файл `conftest.py`
+
 1. Нажмите **Add file** → **Create new file**.
 2. В поле для имени файла введите: `conftest.py`.
 3. В тело файла добавьте:
@@ -8,9 +8,7 @@
 
    @pytest.fixture(scope="function")
    def browser():
-       # Инициализация драйвера
-       driver = webdriver.Chrome()  # Укажите путь к драйверу, если он не в PATH
+       driver = webdriver.Chrome()  
        yield driver
-       # Закрытие драйвера после теста
        driver.quit()
    
