e0f07bceecfbee144364368e85f3325325a640d7
Евгений <enbob.tester@gmail.com>
2025-02-25 17:32:07 +0300

    test_page_dialogs.py

--- tests/test_page_dialogs.py
+++ tests/test_page_dialogs.py
@@ -1,22 +1,3 @@
-# import pytest
-# from selenium import webdriver
-# from pages.modal_dialogs import ModalDialogsPage
-
-# @pytest.fixture
-# def browser():
-#     driver = webdriver.Chrome()
-#     yield driver
-#     driver.quit()
-
-# def test_modal_elements(browser):
-#     modal_page = ModalDialogsPage(browser)
-
-#     modal_page.open()
-
-#     buttons_count = modal_page.get_buttons_count()
-#     assert buttons_count == 5, f"Ожидалось 5 кнопок, но найдено {buttons_count}"
-
-
 import pytest
 from selenium import webdriver
 from pages.modal_dialogs import ModalDialogsPage
