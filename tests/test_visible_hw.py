3cb46ff80896528a23518473c7b77bb09988ac4b
Евгений <enbob.tester@gmail.com>
2025-02-26 11:48:20 +0300

    test_visible_hw.py

--- tests/test_visible_hw.py
+++ tests/test_visible_hw.py
@@ -5,7 +5,7 @@ from pages.accordion import Accordion
 
 @pytest.fixture
 def browser():
-    driver = webdriver.Chrome()  # или любой другой браузер
+    driver = webdriver.Chrome()
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
@@ -22,23 +22,10 @@ def test_visible_accordion(browser):
 
     assert not accordion_page.section1_content().is_displayed(), "Элемент #section1Content > p не должен быть виден после клика"
 
-
-import pytest
-from selenium import webdriver
-from pages.accordion import Accordion
-
-@pytest.fixture
-def browser():
-    driver = webdriver.Chrome()  # или любой другой браузер
-    driver.implicitly_wait(10)
-    yield driver
-    driver.quit()
-
 def test_visible_accordion_default(browser):
     accordion_page = Accordion(browser)
     accordion_page.open()
 
-    # Проверка, что элементы скрыты по умолчанию
     assert not accordion_page.section2_content_p1().is_displayed(), "Элемент #section2Content > p:nth-child(1) должен быть скрыт"
     assert not accordion_page.section2_content_p2().is_displayed(), "Элемент #section2Content > p:nth-child(2) должен быть скрыт"
     assert not accordion_page.section3_content_p().is_displayed(), "Элемент #section3Content > p должен быть скрыт"
